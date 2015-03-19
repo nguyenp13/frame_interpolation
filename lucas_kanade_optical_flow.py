dac2deb312afba0235d5b4dddb9e446fe35b6d8f
nguyenp13 <pn2yr@virginia.edu>
2015-03-19 14:26:45 -0400

    Lucas-Kanade Optical Flow - Did some refactoring

--- lucas_kanade_optical_flow.py
+++ lucas_kanade_optical_flow.py
@@ -13,33 +13,35 @@ import scipy.ndimage.filters
 # Non-Standard Libraries
 from util import *
 
-KERNEL_DIM = 5
-
 def usage():
-    # Sample Usage: python lucas_kanade_optical_flow.py a.jpg b.jpg 5 out.pfm
-    print >> sys.stderr, 'python '+__file__+' image_a image_b spatial_sigma output_pfm'
+    # Sample Usage: python lucas_kanade_optical_flow.py a.jpg b.jpg out.pfm
+    print >> sys.stderr, 'python '+__file__+' image_a image_b output_pfm'
+    print >> sys.stderr, ''
+    print >> sys.stderr, 'Options: -spatial_sigma <float_val>'
+    print >> sys.stderr, '         -kernel_dim <int_val>'
+    print >> sys.stderr, ''
     sys.exit(1)
 
-def main():
-    numpy.set_printoptions(linewidth=200, precision=5, suppress=True)
-    if len(sys.argv) < 5:
-        usage()
-    print 
-    
-    A = numpy.array(Image.open(sys.argv[1])).astype('float')
-    B = numpy.array(Image.open(sys.argv[2])).astype('float')
-    spatial_sigma = float(sys.argv[3])
-    out_pfm_file_name = os.path.abspath(sys.argv[4])
-    
-    A_grayscale = convert_to_grayscale(A)
-    B_grayscale = convert_to_grayscale(B)
-    
+def calculate_velocity(weighted_sum_of_squared_x_derivative, weighted_sum_of_squared_y_derivative, weighted_sum_of_xy_derivative, weighted_sum_of_xt_derivative, weighted_sum_of_yt_derivative):
+    ans = numpy.dot( 
+                      numpy.linalg.pinv( numpy.array(
+                                                      [ [weighted_sum_of_squared_x_derivative, weighted_sum_of_xy_derivative] ,
+                                                        [weighted_sum_of_xy_derivative, weighted_sum_of_squared_y_derivative] ]
+                                                    )
+                                       )
+                     ,
+                     numpy.array([-weighted_sum_of_xt_derivative, -weighted_sum_of_yt_derivative])
+                   )
+    V_x = ans[0]
+    V_y = ans[1]
+    return V_x, V_y
+
+calculate_velocity_vectorized = numpy.vectorize(calculate_velocity)
+
+def lucas_kanade_simple(A_grayscale, B_grayscale, gaussian_kernel):
     Ax = scipy.ndimage.filters.sobel(A_grayscale,axis=1)
     Ay = scipy.ndimage.filters.sobel(A_grayscale,axis=0)
     At = B_grayscale-A_grayscale
-    
-    gaussian_kernel = get_gaussian_kernel(KERNEL_DIM, spatial_sigma) # To use Gaussian weights
-#    gaussian_kernel = numpy.ones([KERNEL_DIM,KERNEL_DIM], dtype='float') # To use no weights
     Ax_squared = numpy.square(Ax)
     Ay_squared = numpy.square(Ay)
     Ax_squared_blurred = convolve(Ax_squared, gaussian_kernel, zero_borders=True)
@@ -52,32 +54,36 @@ def main():
     Axt_blurred = convolve(Axt, gaussian_kernel, zero_borders=True)
     Ayt_blurred = convolve(Axt, gaussian_kernel, zero_borders=True)
     
-    def calculate_velocity(weighted_sum_of_squared_x_derivative, weighted_sum_of_squared_y_derivative, weighted_sum_of_xy_derivative, weighted_sum_of_xt_derivative, weighted_sum_of_yt_derivative):
-        ans = numpy.dot( 
-                          numpy.linalg.pinv( numpy.array(
-                                                          [ [weighted_sum_of_squared_x_derivative, weighted_sum_of_xy_derivative] ,
-                                                            [weighted_sum_of_xy_derivative, weighted_sum_of_squared_y_derivative] ]
-                                                        )
-                                           )
-                         ,
-                         numpy.array([-weighted_sum_of_xt_derivative, -weighted_sum_of_yt_derivative])
-                       )
-        V_x = ans[0]
-        V_y = ans[1]
-        return V_x, V_y
-    
-    calculate_velocity_vectorized = numpy.vectorize(calculate_velocity)
-    
-    velocity_map = numpy.empty(A.shape, dtype='float')
-    
+    velocity_map = numpy.empty(A_grayscale.shape+(3,), dtype='float')
     velocity_map[:,:,0],velocity_map[:,:,1] = calculate_velocity_vectorized(Ax_squared_blurred, Ay_squared_blurred, Axy_blurred, Axt_blurred, Ayt_blurred)
+    
+    return velocity_map
+
+def main():
+    numpy.set_printoptions(linewidth=200, precision=5, suppress=True)
+    if len(sys.argv) < 4:
+        usage()
+    print 
+    
+    A = numpy.array(Image.open(sys.argv[1])).astype('float')
+    B = numpy.array(Image.open(sys.argv[2])).astype('float')
+    out_pfm_file_name = os.path.abspath(sys.argv[3])
+    spatial_sigma = float(get_command_line_param_val(sys.argv, '-spatial_sigma', 'Error: A spatial sigma must be specified.', 'Error: Problem with specified spatial sigma value.'))
+    kernel_dim = int(get_command_line_param_val(sys.argv, '-kernel_dim', 'Error: Kernel width must be specified.', 'Error: Problem with specified kernel width.'))
+    
+    A_grayscale = convert_to_grayscale(A)
+    B_grayscale = convert_to_grayscale(B)
+    
+    gaussian_kernel = get_gaussian_kernel(kernel_dim, spatial_sigma) # To use Gaussian weights
+#    gaussian_kernel = numpy.ones([kernel_dim,kernel_dim], dtype='float') # To use no weights
+    velocity_map = lucas_kanade_simple(A_grayscale, B_grayscale, gaussian_kernel)
     
     save_image(numpy.square(velocity_map[:,:,0]),'V_x.png')
     save_image(numpy.square(velocity_map[:,:,1]),'V_y.png')
     
     writepfm(velocity_map, out_pfm_file_name)
     
-    pdb.set_trace()
+#    pdb.set_trace()
 
 if __name__ == '__main__':
     main()
